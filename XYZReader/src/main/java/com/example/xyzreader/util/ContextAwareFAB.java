8bacef4b3a1158d945180eacb28a6a047d7ed0a3
Andres Solano <radsen@Andress-MacBook-Pro-4.local>
2017-06-20 22:47:20 -0500

    Changing to the new URL to retrieve the data

---
+++ XYZReader/src/main/java/com/example/xyzreader/util/ContextAwareFAB.java
@@ -0,0 +1,10 @@
+package com.example.xyzreader.util;
+
+import android.support.design.widget.CoordinatorLayout;
+
+/**
+ * Created by radsen on 6/20/17.
+ */
+
+public class ContextAwareFAB extends CoordinatorLayout.Behavior {
+}
